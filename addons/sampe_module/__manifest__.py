f114f17a9a21989c202079eb613d050d55d4f785
Ady Shaputra <adyshaputra78@gmail.com>
2025-04-17 15:40:35 +0700

    first commit

---
+++ addons/sampe_module/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Sampe Module",
+    'version': "1.0",
+    'author': "Ady Shaputra",
+    'category': "Tools",
+    'description': "Sample for module",
+    'depends': ['base'],
+    'application': True,
+    'installable': True
+}
